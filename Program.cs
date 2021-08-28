dc2c698c1a0f81740643120da13ec9d2d64315f9
LuizaVanelli <luizavanelli.vanelli@gmail.com>
2021-08-27 22:03:15 -0300

    Atividadede Ola Mundo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.ForegroundColor = ConsoleColor.Cyan;
+           Console.WriteLine();
+           Console.WriteLine("Olá Usuário eu me chamo Luiza Gonçales Vanelli");
+        }
+    }
+}
